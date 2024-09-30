91fc5174571bf4b48e9e75793f0ac3baf0fa8b62
trngnneee <dtnguyen23@clc.fitus.edu.vn>
2024-09-30 18:25:47 +0700

    Finish topHeader, Header, Section1 + Reponsive

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+    theme: {
+        screens: {
+          'sm': '576px',
+          'md': '768px',
+          'lg': '992px',
+          'xl': '1240px',
+          '2xl': '1240px',
+        },
+
+        extend: {
+            colors: {
+                primary: '#000000',
+                secondary: '#00000099',
+            }
+          },
+
+        fontFamily: {
+            BeVN: ['Be Vietnam Pro', 'sans-serif'],
+        }  
+      }
+}
